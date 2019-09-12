584cafba6a2814801bed2dc56419728e6901135c
Puttipong1234 <Puttipong.lims@gmail.com>
2019-09-13 01:08:55 +0700

    "PART3 FINISH"

--- Project/FlexTemplate.py
+++ Project/FlexTemplate.py
@@ -1,3 +1,4 @@
+
 flexdata = {
   "type": "flex",
   "altText": "Flex Message",
@@ -138,6 +139,153 @@ flexdata = {
       }
     ]
   }
+}
+
+flexdata_regis = {
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": 
+{
+  "type": "bubble",
+  "size": "kilo",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "image",
+        "url": "https://scontent.fbkk5-8.fna.fbcdn.net/v/t31.0-8/19466423_820149698158102_8686134731311075458_o.jpg?_nc_cat=106&_nc_oc=AQms9LwmxwqmviVC5BgT1cPTnzERW6ki3k4AVBMyfgkzR7l01VDM1-K5UUB7n9AkBZU&_nc_ht=scontent.fbkk5-8.fna&oh=d58e7507b94709a6c2b8f482c799df12&oe=5DFD6F52",
+        "size": "full",
+        "gravity": "top",
+        "aspectMode": "cover"
+      },
+      {
+        "type": "text",
+        "text": "LINE CHATBOT WITH PYTHON",
+        "position": "absolute",
+        "offsetStart": "7px",
+        "size": "md",
+        "weight": "bold",
+        "style": "italic",
+        "offsetTop": "7px",
+        "color": "#ffffff"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "filler"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "filler",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "ลงทะเบียนที่นี้",
+                "size": "xl",
+                "wrap": False,
+                "flex": 3,
+                "align": "center",
+                "color": "#ffffff"
+              },
+              {
+                "type": "filler",
+                "flex": 1
+              }
+            ]
+          },
+          {
+            "type": "filler"
+          }
+        ],
+        "spacing": "sm",
+        "margin": "xxl",
+        "height": "60px",
+        "borderWidth": "3px",
+        "borderColor": "#ffffff",
+        "cornerRadius": "5px",
+        "action": {
+          "type": "uri",
+          "uri": "http://linecorp.com/",
+          "altUri": {
+            "desktop": "http://linecorp.com/"
+          },
+          "label": "regis"
+        }
+      },
+      {
+        "type": "image",
+        "url": "https://engineering.fb.com/wp-content/uploads/2016/05/2000px-Python-logo-notext.svg_.png",
+        "position": "absolute",
+        "size": "xxs",
+        "offsetTop": "200px",
+        "aspectRatio": "1:1"
+      },
+      {
+        "type": "image",
+        "url": "https://lh3.googleusercontent.com/l-ZZOFGyeKYz3stUbxTECHYnXcRD66C9g0tjiWA_okVIxZyb0E7_esU8LRpq_0LFCu8Y",
+        "position": "absolute",
+        "size": "xxs",
+        "offsetTop": "200px",
+        "offsetStart": "30px"
+      },
+      {
+        "type": "image",
+        "url": "https://pbs.twimg.com/profile_images/689189555765784576/3wgIDj3j.png",
+        "position": "absolute",
+        "size": "xxs",
+        "offsetTop": "200px",
+        "offsetStart": "60px"
+      },
+      {
+        "type": "image",
+        "url": "https://image-dev.wi.th/uploads/images/profiles/SJRX-EkVZ",
+        "position": "absolute",
+        "size": "xxs",
+        "offsetTop": "200px",
+        "offsetStart": "90px"
+      },
+      {
+        "type": "image",
+        "url": "https://png2.cleanpng.com/sh/e9f13a7f41c6f4829c32750437b4a456/L0KzQYm3VcA5N6lmj5H0aYP2gLBuTfZtaaRwReJEdHjyfn7AhfIubqNmhddCb4LuPcjsgr1ieKFxgdVqdHnyfn77lgRweppmRadqZkHncrOAU8U3PmU9Rqk8NkW2Q4OAUcUzPWgAUaoBM0K0SIi1kP5o/kisspng-flask-python-web-framework-web-application-tutoria-5af1dbb7356648.7365332715257998632187.png",
+        "position": "absolute",
+        "size": "xxs",
+        "offsetTop": "200px",
+        "offsetStart": "120px"
+      },
+      {
+        "type": "image",
+        "url": "https://png2.cleanpng.com/sh/93d2d8af2857089b4b9c76d8e88fbbfd/L0KzQYm3VcI2N6V2epH0aYP2gLBuTfdqfJl6ep9ybnOwgrb3jCNqfJD3kZ98b4X1c7a0gB9lbV46eqIDN3K7R7O6UsM3QV88UaI8NEizRoK8UsczQGM6TqkCM0O7PsH1h5==/kisspng-github-inc-repository-source-code-5b087b87b32369.7903480615272825677338.png",
+        "position": "absolute",
+        "size": "xxs",
+        "offsetTop": "200px",
+        "offsetStart": "150px"
+      },
+      {
+        "type": "image",
+        "url": "https://image.flaticon.com/icons/svg/1612/1612781.svg",
+        "position": "absolute",
+        "offsetEnd": "10px",
+        "offsetBottom": "10px",
+        "size": "xxs"
+      }
+    ],
+    "paddingAll": "0px",
+    "backgroundColor": "#000000",
+    "borderWidth": "7px"
+  },
+  "styles": {
+    "body": {
+      "backgroundColor": "#00000000"
+    }
+  }
+}
 }
 
 import json , requests
